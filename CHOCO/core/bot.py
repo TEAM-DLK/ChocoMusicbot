f872d8a93457ebadde95c65201cffba6dcb92b1d
DOOZY LAB LK <93440107+TEAM-DLK@users.noreply.github.com>
2024-02-17 19:43:46 +0530

    Update bot.py

--- CHOCO/core/bot.py
+++ CHOCO/core/bot.py
@@ -8,11 +8,11 @@ from CHOCO import config
 from ..logging import LOGGER
 
 
-class CHOCOBot(Client):
+class ChocoBot(Client):
     def __init__(self):       
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            name="CHOCOX",
+            name="ChoX",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
